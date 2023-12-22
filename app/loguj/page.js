7a612a5f986a1255725b9d54fba8baebdac77618
Jaroslaw Matusiak <67241739+MilTech124@users.noreply.github.com>
2023-12-22 17:31:50 +0100

    https

--- app/loguj/page.js
+++ app/loguj/page.js
@@ -6,6 +6,7 @@ function page() {
         <p className='md:text-5xl'>Zaloguj się do panelu administracyjnego</p>
         <a href="https://backend.premiumetal.pl/backed/wp-admin/post.php?post=8&action=edit" target="_blank" rel="noopener noreferrer"><button className='btn btn-primary'>Galeria</button></a>
         <a href="https://backend.premiumetal.pl/backed/wp-admin/post.php?post=60&action=edit" target="_blank" rel="noopener noreferrer"><button className='btn btn-primary'>Śmietniki</button></a>
+        <a href="https://backend.premiumetal.pl/backed/wp-admin/post.php?post=66&action=edit" target="_blank" rel="noopener noreferrer"><button className='btn btn-primary'>Głowna</button></a>
     </div>
   )
 }
