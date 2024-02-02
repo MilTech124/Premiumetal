50bdbe151772bd6224ce5b5d1047dfa2087bc776
Jaroslaw Matusiak <67241739+MilTech124@users.noreply.github.com>
2024-02-02 19:03:58 +0100

    upload

--- app/components/Header.js
+++ app/components/Header.js
@@ -21,9 +21,10 @@ function Header() {
                 <li onClick={()=>{setIsOpen(false)}}> <Link href="/smietniki">Wiaty Śmietnikowe</Link></li>
                 <li onClick={()=>{setIsOpen(false)}}> <Link href="/#ofirmie">O firmie</Link></li>
                 {/* ON HOVER DROPDOWN */}
-                <li  onClick={()=>{setIsOpen(false)}}className='relative group'>Poradnik
+                <li className='relative group'>Poradnik
                 <div className='absolute opacity-0 hidden text-sm group-hover:flex  group-hover:opacity-100 top-7 -left-5 bg-red w-[150px] '>
                     <ul className='flex flex-col gap-5 p-5'>
+                        <li onClick={()=>{setIsOpen(false)}}> <Link href="/akcesoria">Dodatkowe akcesoria</Link></li>
                         <li onClick={()=>{setIsOpen(false)}}> <Link href="/przygotowanie-podloza">Przygotowanie podłoża</Link></li>
                         <li onClick={()=>{setIsOpen(false)}}> <Link href="/paleta-kolorow">Paleta kolorów</Link></li>
                         <li onClick={()=>{setIsOpen(false)}}> <Link href="/informacje">Informacje</Link></li>
