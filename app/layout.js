3c8199da4f11dd904639c518be7b45fb9a7991cb
Jaroslaw Matusiak <67241739+MilTech124@users.noreply.github.com>
2024-03-25 16:44:38 +0100

    pl

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pl" className='scroll-smooth' style={{scrollBehavior:'smooth'}}>    
+    <head>
+      <script dangerouslySetInnerHTML={{ __html: `
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16510752046">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-16510752046');
+</script>` }} />
+
+
+    </head>
+
       <body className={inter.className} >
       <Header />
       {children}
