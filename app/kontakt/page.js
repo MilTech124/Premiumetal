eb3a53dc26db59fa2dd1c0328795d05a9861b6b3
Jaroslaw Matusiak <67241739+MilTech124@users.noreply.github.com>
2024-04-02 17:18:08 +0200

    pl revalidate

--- app/kontakt/page.js
+++ app/kontakt/page.js
@@ -1,86 +1,76 @@
-'use client'
+"use client";
 import { Fade } from "react-awesome-reveal";
-import Script from 'next/script';
+import Script from "next/script";
 
 function page() {
   return (
     <>
       <Script id="click" strategy="lazyOnload">
         {`
-        function gtag_report_conversion(url) {
-          var callback = function () {
-            if (typeof(url) != 'undefined') {
-              window.location = url;
-            }
-          };
-          gtag('event', 'conversion', {
-              'send_to': 'AW-16510752046/bljbCOyUsKAZEK6q-MA9',
-              'event_callback': callback
-          });
-          return false;
-        }
-        `}
+          gtag('event', 'conversion', {'send_to': 'AW-16510752046/bljbCOyUsKAZEK6q-MA9'});
+        `}      
+
       </Script>
-    <main className="bg-[url(/images/bg-kontakt.png)] bg-cover flex flex-col bg-no-repeat  bg-center py-10  md:pl-[10vw]">
-      <h1 className="text-slate-100">Kontakt</h1>
+      <main className="bg-[url(/images/bg-kontakt.png)] bg-cover flex flex-col bg-no-repeat  bg-center py-10  md:pl-[10vw]">
+        <h1 className="text-slate-100">Kontakt</h1>
 
-      <div className="py-20 flex flex-col gap-10">
-        {/* ITEM */}
-        <Fade>
-        <div className="flex gap-2 group w-fit">
-          <div className=" flex justify-center items-center h-20 w-20 rounded-full bg-primary group-hover:bg-red-600  group-hover:rotate-12 transition-all duration-700">
-            <img src="/images/cellphone.png" alt="mobile" className="p-2" />
-          </div>
-          <div className="flex gap-2 flex-col items-center justify-center bg-primary rounded-md px-10 font-semibold group w-[250px]">
-            <a href="tel:+48 517330176">+48 517 330 176</a>
-            <a href="tel:+48 665983244">+48 665 983 244</a>
-          </div>
-        </div>
-        </Fade>
-        {/* ITEM */}
-        {/* ITEM */}
-        <Fade>
-        <div className="flex gap-2 group w-fit">
-          <div className=" flex justify-center items-center h-20 w-20 rounded-full bg-primary group-hover:bg-red-600  group-hover:rotate-12 transition-all duration-700">
-            <img src="/images/Email.png" alt="mobile" className="p-2" />
-          </div>
-          <div className="flex gap-2 flex-col items-center justify-center bg-primary rounded-md px-10 font-semibold group w-[250px]">
-            <a href="mailto:garaze@premiumetal.pl">garaze@premiumetal.pl</a>
-          </div>
+        <div className="py-20 flex flex-col gap-10">
+          {/* ITEM */}
+          <Fade>
+            <div className="flex gap-2 group w-fit">
+              <div className=" flex justify-center items-center h-20 w-20 rounded-full bg-primary group-hover:bg-red-600  group-hover:rotate-12 transition-all duration-700">
+                <img src="/images/cellphone.png" alt="mobile" className="p-2" />
+              </div>
+              <div className="flex gap-2 flex-col items-center justify-center bg-primary rounded-md px-10 font-semibold group w-[250px]">
+                <a href="tel:+48 517330176">+48 517 330 176</a>
+                <a href="tel:+48 665983244">+48 665 983 244</a>
+              </div>
+            </div>
+          </Fade>
+          {/* ITEM */}
+          {/* ITEM */}
+          <Fade>
+            <div className="flex gap-2 group w-fit">
+              <div className=" flex justify-center items-center h-20 w-20 rounded-full bg-primary group-hover:bg-red-600  group-hover:rotate-12 transition-all duration-700">
+                <img src="/images/Email.png" alt="mobile" className="p-2" />
+              </div>
+              <div className="flex gap-2 flex-col items-center justify-center bg-primary rounded-md px-10 font-semibold group w-[250px]">
+                <a href="mailto:garaze@premiumetal.pl">garaze@premiumetal.pl</a>
+              </div>
+            </div>
+          </Fade>
 
-        </div>
-        </Fade>
-       
-        {/* ITEM */}
-        {/* ITEM */}
-        <Fade>
-        <div className="flex gap-2 group w-fit">
-          <div className=" flex justify-center items-center h-20 w-20 rounded-full bg-primary group-hover:bg-red-600  group-hover:rotate-12 transition-all duration-700">
-            <img src="/images/Clock.png" alt="mobile" className="p-2" />
-          </div>
-          <div className="flex gap-2 flex-col items-center justify-center bg-primary rounded-md px-10 p-2 font-normal group w-[250px]">
-            <p className="font-semibold">Godziny Pracy:</p>
-            <p>pn-pt: 8:00 -16:00</p>
-            <p>sob: 9:00-13:00</p>
-            <p>niedz: nieczynne</p>
-          </div>
-        </div>
-        </Fade>
-        {/* ITEM */}
+          {/* ITEM */}
+          {/* ITEM */}
+          <Fade>
+            <div className="flex gap-2 group w-fit">
+              <div className=" flex justify-center items-center h-20 w-20 rounded-full bg-primary group-hover:bg-red-600  group-hover:rotate-12 transition-all duration-700">
+                <img src="/images/Clock.png" alt="mobile" className="p-2" />
+              </div>
+              <div className="flex gap-2 flex-col items-center justify-center bg-primary rounded-md px-10 p-2 font-normal group w-[250px]">
+                <p className="font-semibold">Godziny Pracy:</p>
+                <p>pn-pt: 8:00 -16:00</p>
+                <p>sob: 9:00-13:00</p>
+                <p>niedz: nieczynne</p>
+              </div>
+            </div>
+          </Fade>
+          {/* ITEM */}
 
-        {/* ITEM */}
-        <Fade>
-        <div className="bg-primary w-[350px] rounded-md p-5">
-          <p className="font-semibold">Gotowy na Garaż Swoich Marzeń?</p>
-          <p className="font-normal pt-5">
-            Skontaktuj się z nami już dziś, aby omówić Twoje potrzeby i zacząć
-            tworzyć idealny garaż!<br></br> Jesteśmy tutaj, aby pomóc Ci na każdym kroku.
-          </p>
+          {/* ITEM */}
+          <Fade>
+            <div className="bg-primary w-[350px] rounded-md p-5">
+              <p className="font-semibold">Gotowy na Garaż Swoich Marzeń?</p>
+              <p className="font-normal pt-5">
+                Skontaktuj się z nami już dziś, aby omówić Twoje potrzeby i
+                zacząć tworzyć idealny garaż!<br></br> Jesteśmy tutaj, aby pomóc
+                Ci na każdym kroku.
+              </p>
+            </div>
+          </Fade>
+          {/* ITEM */}
         </div>
-        </Fade>
-        {/* ITEM */}
-      </div>
-    </main>
+      </main>
     </>
   );
 }
