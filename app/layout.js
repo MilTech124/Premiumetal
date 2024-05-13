d5f37aa7581e13c75986fee3db2a4934ea2f62b3
Jaroslaw Matusiak <67241739+MilTech124@users.noreply.github.com>
2024-05-13 13:22:20 +0200

    metadata

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,36 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'PREMIUMETAL',
-  description: 'Prémium osztályú garázsok',
-  keywords: 'Fémgarázsok, garázsok, fémházak, kennel, kutyakennel, fém kutyakennel',
+  title: "Garázsok PREMIUMetal -  A legmagasabb minőség az Ön igényeinek!",
+  description: `Fedezze fel prémium garázskínálatunkat az Ön igényeire szabva.
+ Bízzon meg a PREMIUMetalban!`,
+  keywords:
+    "lemezgarázsok, bádoggarázsok, fahatású garázsok, akril garázsok, kennelek, kutya kennelek, prémium garázsok",
+  robots: "index, follow",
   openGraph: {
-    images: '/og-image.png',
+    images: "/og-image.png",
   },
-}
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pl" className='scroll-smooth' style={{scrollBehavior:'smooth'}}>    
-      <body className={inter.className} >
-      <Header />
-      {children}
-      <Footer />
+    <html
+      lang="pl"
+      className="scroll-smooth"
+      style={{ scrollBehavior: "smooth" }}
+    >
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
 
-
-export const revalidate = 3600 // revalidate at most every hour
+export const revalidate = 3600; // revalidate at most every hour
