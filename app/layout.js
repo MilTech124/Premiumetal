896c845e378fc8f62ccf3e6aa7f698fe716a6732
Jaroslaw Matusiak <67241739+MilTech124@users.noreply.github.com>
2024-06-07 14:10:22 +0200

    sk

--- app/layout.js
+++ app/layout.js
@@ -7,9 +7,9 @@ import Script from 'next/script'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Garáže PREMIUMetal - Najvyššia kvalita pre vaše potreby!',
-  description: 'Objavte našu ponuku prémiových garáží, ktoré sú prispôsobené vašim potrebám. Spoľahnite sa na PREMIUMetal!',
-  keywords: 'plechove garaze, plechova garaz, plechové garáže, plechová garáž, záhradný domček na náradie, domček na náradie, domcek na naradie, koterce pre psov, koterce, prístrešok na drevo, pristresok na drevo',
+  title: 'Garáže PREMIUMetal - Nejvyšší kvalita pro vaše potřeby',
+  description: 'Objevte naši nabídku prémiových garáží, které jsou přizpůsobeny vašim potřebám. Spolehněte se na PREMIUMetal!',
+  keywords: 'plechová garáž, plechové garáže, plechova garaz, domky na nářadí, zahradní domky na nářadí, zahradní domky na nářadí z polska, kotec pro psa, kotec pro psa hornbach, kotec, přístřešek, přístřešek na dřevo',
   robots: 'index, follow',
   openGraph: {
     images: '/og-image.png',
